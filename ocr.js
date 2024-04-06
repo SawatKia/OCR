57b51ea6f0c088cc93dde5c99b220da8840ee4b6
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2024-04-06 15:45:30 +0700

    correct the Output  correspond to its field

--- ocr.js
+++ ocr.js
@@ -52,7 +52,7 @@ app.post("/upload", upload.single("fileupload"), async (req, res) => {
     });
 
     // Extract text from image
-    const extractedText = await extractTextFromImage(imageBuffer, rectangles, worker);
+    const extractedText = await extractTextFromImage(imageBuffer, rectangles);
     await worker.terminate();
 
     // Store extracted text in stocks data format
@@ -67,31 +67,50 @@ app.post("/upload", upload.single("fileupload"), async (req, res) => {
 
 app.post("/uploads", upload.fields(imagesfield), async (req, res) => {
   try {
-    if(!req.files) {
+    if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send({ message: "Please upload images" });
     }
-    for(let i = 0;i<req.files.length;i++) {
-      if(!imageMimeTypes.includes(req.files[i].mimetype)) {
+
+    worker = await createWorker("eng", 1, {
+      logger: (m) => {
+        if (m.status === "recognizing text") {
+          const progress = Math.round(m.progress * 100);
+          process.stdout.clearLine();
+          process.stdout.cursorTo(0);
+          const dots = ".".repeat(Math.floor(progress / 5)).padEnd(20, " ");
+          process.stdout.write(`[${dots}] ${progress}%`);
+        } else {
+          console.log(m.status);
+        }
+      },
+      errorHandler: (err) => console.error(err),
+    });
+
+    const extractedTexts = [];
+    const files = Object.values(req.files);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const mimetype = file[0].mimetype;
+      if (!imageMimeTypes.includes(mimetype)) {
         return res.status(400).send({ message: "Please upload a valid image file" });
       }
-      worker = await createWorker("eng", 1, {
-        logger: (m) => {
-          if (m.status === "recognizing text") {
-            const progress = Math.round(m.progress * 100);
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);
-            const dots = ".".repeat(Math.floor(progress / 5)).padEnd(20, " ");
-            process.stdout.write(`[${dots}] ${progress}%`);
-          } else {
-            console.log(m.status);
-          }
-        },
-        errorHandler: (err) => console.error(err),
-      });
+      const extractedText = await extractTextFromImage(file[0].buffer);
+      extractedTexts.push(extractedText);
     }
 
+    await worker.terminate();
+
+    console.log(`extractedText of all images to postprocessing:\n\ttype: ${typeof extractedTexts}\n\ttext: ${extractedTexts}`);
+    // Store extracted text in stocks data format
+    const stocksData = getStocksData(extractedTexts);
+    console.log("stocksData in Endpoint[/uploads]: ", stocksData);
+
+
+    return res.status(200).send({ status: "Upload success", data: stocksData });
   } catch (error) {
-    
+    //FIXME - delete the console log before production
+    //console.error("Error during text extraction:", error);
+    return res.status(500).send({ message: "Internal server error" || error });
   }
 });
 
@@ -99,15 +118,19 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 //SECTION -  Utility functions
 // Extract text from an image using Tesseract
-async function extractTextFromImage(image, rectangles, worker) {
+async function extractTextFromImage(image, rectangles = []) {
   const extractedText = [];
-
-  for (let i = 0; i < rectangles.length; i++) {
-    const { data: { text } } = await worker.recognize(image, { rectangle: rectangles[i] });
-    console.log(` : The text in rectangle ${i + 1} is: \n${text}`);
+  if (rectangles.length === 0) {
+    const { data: { text } } = await worker.recognize(image);
     extractedText.push(text);
+  } else {
+    for (let i = 0; i < rectangles.length; i++) {
+      const { data: { text } } = await worker.recognize(image, { rectangle: rectangles[i] });
+      console.log(` : The text in rectangle ${i + 1} is: \n${text}`);
+      extractedText.push(text);
+    }
   }
-
+  console.log(` extractedText type: ${typeof(extractedText)}`);
   return extractedText;
 }
 
@@ -115,27 +138,34 @@ async function extractTextFromImage(image, rectangles, worker) {
 function storeStocksData(extractedText) {
   const finding = ["Symbol", "volume", "average_price"];
   const stocks = [];
+  const result = [];
 
   for (let i = 0; i < extractedText.length; i++) {
-    const result = postprocessing(extractedText[i], finding[i]);
-    const stockData = {};
-
-    stockData.symbol = result[0] || null;
-    stockData.volume = result[1] || null;
-    stockData.average_price = result[2] || null;
-
-    stocks.push(stockData);
+    result.push(postprocessing(extractedText[i], finding[i]));
   }
-
+  for(let k =0; k < Math.max(result[0].length,result[1].length,result[2].length); k++) {
+    const stockData = {};
+    stockData.symbol = result[0][k];
+    stockData.volume = result[1][k];
+    stockData.average_price = result[2][k];
+    stocks.push(stockData);
+    console.log(`stockData: ${JSON.stringify(stockData)}`);
+  }
   return stocks;
 }
 
+
 function postprocessing(text, find) {
+  // Check if text is not a string
+  if (typeof text !== 'string') {
+    // Convert text to string
+    text = String(text);
+  }
+  console.log(`text type to postprocessing: ${typeof(text)}`)
   const regex = {
     Symbol: /([A-Z]{2,5}\w?)\b/g,
     volume: /\d*\,?\d+/g,
     'average_price': /\d+\.\d+/g
   };
-  console.log(`${find}: ${text.match(regex[find])}` );
   return text.match(regex[find]);
 }
